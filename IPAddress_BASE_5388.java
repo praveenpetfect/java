f5f38914fe8b9c9bba8209ff89b06daf6d140216
praveen <petfect40@gmail.com>
2018-08-14 13:15:31 +0530

    afsfe

--- IPAddress_BASE_5388.java
+++ IPAddress_BASE_5388.java
@@ -4,10 +4,6 @@ class IPAddress
 public static void main(String args[]) throws Exception
 {
 System.out.println(InetAddress.getLocalHost());
-<<<<<<< HEAD
-  System.out.println("i had a dove");
-=======
-  System.out.println("i had dog");
->>>>>>> b9c1223ef4da8e0e2b303fc7f3d10988ccc52ff4
+  System.out.println("lol lol lol");
 }
 }
